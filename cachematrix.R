68f1b871c797ef7999fb61fed222af69a7e97d6b
mtascon <course.mta@gmail.com>
2014-06-18 19:43:24 +0200

    First makeCacheMatrix
    makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,30 @@
 
 ## Write a short comment describing this function
 ## Comment to test commit in GitHub
+## This makeCacheMatrix is based on makeVector 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    m <- NULL
+    set <- function(y) {
+      x  <<- y
+      m <<- NULL
+    }
+    get <- function() {
+      x
+    }
+    setinverse <- function(inverse)
+    {
+      m <<- inverse
+    }  
+    getinverse <- function()
+    {
+      m
+    }  
+    list(set = set, get = get,
+         getinverse = setinverse,
+         getinverse = getinverse)
+  
 }
 
 
