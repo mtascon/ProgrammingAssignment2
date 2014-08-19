9d23da780e4bec4d0ab0e3f4b778cdabfa794122
mtascon <course.mta@gmail.com>
2014-08-19 12:15:48 +0200

    Final Version
    Fixed Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Comment to test commit in GitHub
 
 ## This makeCacheMatrix is based on makeVector 
 ## cachematrix.R defines two functions:
-## 1. makeCacheMatrix fuction defines  matrix object and defines methods to set and get 
-##    this matrix object and its inverse
+## 1. makeCacheMatrix fuction defines  matrix object and 
+##    defines methods to set and get this matrix object 
+##    and its inverse
+## 2. cacheSolve function computes the inverse of the matrix 
+##    returned by makeCacheMatrix
+##    If the inverse has been calculated and the matrix has 
+##    not changed, then the cached inverse is retrieved
+
+## makeCacheMatrix. Function to create and store the matrix 
+## object to cache the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -40,15 +45,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-## Function to calculate the inverse of a matrix
+## cacheSolve. Function to calculate the inverse of a matrix
 ## If the inverse has already been calculated and the 
 ## input matrix has not changes this function retrieves
 ## the inverse from the cache
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-		inv <- x$getinverse()
+    inv <- x$getinverse()
 		
 		## Get cached Matrix and return
 		if(!is.null(inv))
@@ -58,14 +62,16 @@ cacheSolve <- function(x, ...) {
 		}
 		## If it is null we need to calculate the inverse of the matrix
     message("Inverse is null. Calculating new inverse of the matrix")
+    
     ## Get the new Matrix
 		myMatrix <- x$get()
     
     ## Solve function to calculrate inverse
 		inverseMatrix <- solve(myMatrix)
-		## Store the inverse of the matrix
+		
+    ## Store the inverse of the matrix
 		x$setinverse(inverseMatrix)
+    
     ## Return the inverse of the matrix 
 		inverseMatrix
-    return(inverseMatrix)
 }
